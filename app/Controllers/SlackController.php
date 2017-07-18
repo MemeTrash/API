7ae2a82b22636775c39421ab36b599f872ed5a02
Jack Romo <sharrackor@gmail.com>
2017-07-18 21:36:12 +0100

    minor slack fix

--- app/Controllers/SlackController.php
+++ app/Controllers/SlackController.php
@@ -80,9 +80,11 @@ class SlackController extends Controller
     protected function generate(GeneratorInterface $inner, string $text)
     {
         $generator = new ValidatingGenerator($inner);
+        $image = $generator->generate($text);
+        $imageurl = "https://api.memetrash.co.uk/result/{$image}.jpg";
 
         return new JsonResponse([
-            'text' => $generator->generate($text),
+            'text' => $imageurl,
         ]);
     }
 }
